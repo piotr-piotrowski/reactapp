665ce4ea4ea3e8c4a86e7d9f7e2936f5076955c9
Piotr Piotrowski <piotr.j.piotrowski@gmaill.com>
2024-12-31 20:46:21 +0100

    20.5 Creating the data store

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import 'bootstrap/dist/css/bootstrap.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.css";
+import { Provider } from "react-redux";
+import { dataStore } from "./data/dataStore";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={dataStore}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
